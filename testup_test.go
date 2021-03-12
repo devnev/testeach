c870ba163b62aa31effd0bc46c6f78b9740477cb
Mark Nevill <mark.nevill@gmail.com>
2021-03-12 21:30:14 +0000

    Use separate tests for cleaner self-test output.

--- testup_test.go
+++ testup_test.go
@@ -2,9 +2,18 @@ package testeach_test
 
 import "testing"
 
-func TestSuite(t *testing.T) {
+func TestSuite_noCases(t *testing.T) {
 	testNoCases(t)
+}
+
+func TestSuite_twoCases(t *testing.T) {
 	testTwoCases(t)
+}
+
+func TestSuite_nestedCases(t *testing.T) {
 	testNestedCases(t)
+}
+
+func TestSuite_osStat(t *testing.T) {
 	testOsStat(t)
 }
